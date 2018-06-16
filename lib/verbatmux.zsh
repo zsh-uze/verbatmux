3d16271d6b5e705d1d7b3460d58fb308d31716a8
Marc Chantreux <khatar@phear.org>
2018-06-16 12:08:38 +0200

    improve experience

--- lib/verbatmux.zsh
+++ lib/verbatmux.zsh
@@ -1,19 +1,21 @@
 # todo: should be in /bin? need to define what an app is
 
 verbatmux/new-pane () {
-    tmux split-window -bhvl${1:-20} 'uze verbatmux; verbatmux/start'
+	tmux split-window -bhvl${1:-20} 'uze verbatmux; verbatmux/start'
 }
 
 verbatmux/start () {
 	local c m
 	while {read -sk1 c} {
 		case $c {
-			$'\n')   print  ⏎ ; c=c-m ;;
-			$'\e')   print -nP '%F{blue}[ESC]%f'   ;;
-			\ )      print -P  '%F{blue}[space]%f' ;;
-			$'\x7f') print -n $'\b'                ;; # backspace
-			)      clear ; break                 ;;
-			*)       print -n $c                   ;;
+			$'\n')   print -P ' %F{blue}⏎%f' ; c=c-m  ;;
+			$'\e')   print -P ' %F{blue}\\e%f'        ;;
+			$'\t')   print -P ' %F{blue}[tab]%f'      ;;
+			\ )      print -P ' %F{blue}[space]%f'    ;;
+			)      print -P ' %F{blue}^W%f'         ;;
+			$'\x7f') print -n $'\b'                   ;; # backspace
+			)      clear ; break                    ;;
+			*)       print -n $c                      ;;
 		}
 		tmux send -t${1:-1} $c
 	}
